4692aa7fded86d33c0312fdc36f6d51582c3be46
marioalbertoceronf <marioceron@gmail.com>
2019-11-07 17:53:13 -0500

    Added app.py file updated db string connection

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ my_db      = os.environ.get('DB_NAME')
 my_user    = os.environ.get('DB_USER')
 my_passwd  = os.environ.get('DB_USER_PASSWORD')
 my_db_port = os.environ.get('DB_PORT')
-my_host_endpoint= = os.environ.get('DB_ENDPOINT_HOST')
+my_host_endpoint =  os.environ.get('DB_ENDPOINT_HOST')
 
 sql_conn_str='postgresql://'+my_user+':'+my_passwd+'@'+my_rds_host_endpoint+':'+my_db_port+'/'+my_db
 
